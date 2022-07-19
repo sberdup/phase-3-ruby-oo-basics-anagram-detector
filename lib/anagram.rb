f27e5d0d58b20ac13496b4478d7b24413abad1de
Sergio Berdeja <sergioberdeja@gmail.com>
2022-07-18 18:49:18 -0600

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.filter do |string|
+            string.chars.sort == @word.chars.sort
+        end
+    end
+end
